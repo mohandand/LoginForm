0521e8d1fa8d735089136dbbbf0bd447ae9945d0
mohandand <mohandand@gmail.com>
2023-08-05 18:44:25 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# LoginForm
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-2sylgv)
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div className="container">
+      <form className="form">
+        <h4>LoginForm</h4>
+      </form>
+    </div>
+  );
+}
